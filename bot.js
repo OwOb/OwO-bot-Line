cae79ac7fc19df8fe328e71bb6e32e885187f5cf
OwOb <35055831+OwOb@users.noreply.github.com>
2019-12-26 16:23:29 +0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,4 +1,6 @@
 var linebot = require('linebot');
+const express = require('express');
+const bodyParser = require('body-parser');
 //require('dotenv').config();
  
 var bot = linebot({
@@ -6,22 +8,31 @@ var bot = linebot({
   channelSecret: '22edb3562a89f30e25c1917510187e5d',//process.env.CHANNEL_SECRET,
   channelAccessToken: 'X4hPtwmB6ZShzqSeUjsk7jZSMie9SN2iBlKNPupjGLqsrYcIiilmTjk8nbUnZD8126C61MbonpTLQ3x798KlCSDpjDMlrfeFTlGXUX13Jw9C9D+EBOuZ5xZp0CwJ7L2esDIzpTLozjn1qWyY/YJNUwdB04t89/1O/w1cDnyilFU='//process.env.CHANNEL_ACCESS_TOKEN
 });
- 
+
+const app = express();
+
+const parser = bodyParser.json({
+  verify: function (req, res, buf, encoding) {
+    req.rawBody = buf.toString(encoding);
+  }
+});
+
+app.post('/linewebhook', parser, function (req, res) {
+  if (!bot.verify(req.rawBody, req.get('X-Line-Signature'))) {
+    return res.sendStatus(400);
+  }
+  bot.parse(req.body);
+  return res.json({});
+});
+
 bot.on('message', function (event) {
-  console.log('A');
-  // event.message.text是使用者傳給bot的訊息
-  // 使用event.reply(要回傳的訊息)方法可將訊息回傳給使用者
   event.reply(event.message.text).then(function (data) {
-	  console.log('Y');
-	  console.log(data);
-    // 當訊息成功回傳後的處理
+    console.log('Success', data);
   }).catch(function (error) {
-	  console.log('error');
-	  console.log(error);
-    // 當訊息回傳失敗後的處理
+    console.log('Error', error);
   });
 });
- 
-bot.listen('/linewebhook', 3000, function () {
-    console.log('[BOT已準備就緒]');
+
+app.listen(process.env.PORT || 80, function () {
+  console.log('LineBot is running.');
 });
