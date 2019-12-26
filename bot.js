1174d41694b31f243bf47580cce8b220af67605d
OwOb <35055831+OwOb@users.noreply.github.com>
2019-12-26 15:46:50 +0800

    Add files via upload

--- bot.js
+++ bot.js
@@ -1,19 +1,25 @@
 var linebot = require('linebot');
-require('dotenv').config();
+//require('dotenv').config();
  
 var bot = linebot({
-  channelId: process.env.CHANNEL_ID,
-  channelSecret: process.env.CHANNEL_SECRET,
-  channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN
+  channelId: '1653707396',//process.env.CHANNEL_ID,
+  channelSecret: '22edb3562a89f30e25c1917510187e5d',//process.env.CHANNEL_SECRET,
+  channelAccessToken: 'X4hPtwmB6ZShzqSeUjsk7jZSMie9SN2iBlKNPupjGLqsrYcIiilmTjk8nbUnZD8126C61MbonpTLQ3x798KlCSDpjDMlrfeFTlGXUX13Jw9C9D+EBOuZ5xZp0CwJ7L2esDIzpTLozjn1qWyY/YJNUwdB04t89/1O/w1cDnyilFU='//process.env.CHANNEL_ACCESS_TOKEN
 });
  
 bot.on('message', function (event) {
+  console.log('A');
+  // event.message.text是使用者傳給bot的訊息
+  // 使用event.reply(要回傳的訊息)方法可將訊息回傳給使用者
   event.reply(event.message.text).then(function (data) {
-	console.log('OwO');
-    // success
+	  console.log(data);
+    // 當訊息成功回傳後的處理
   }).catch(function (error) {
-    // error
+	  console.log(error);
+    // 當訊息回傳失敗後的處理
   });
 });
  
-bot.listen('/linewebhook', 3000);
+bot.listen('/linewebhook', 3000, function () {
+    console.log('[BOT已準備就緒]');
+});
